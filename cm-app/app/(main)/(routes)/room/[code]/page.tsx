cf471914aec9fcc5570f42d9aaaa823fa359afae
Omkara07 <singh.omkara07@gmail.com>
2025-09-03 01:27:40 +0530

    params await error fix

--- cm-app/app/(main)/(routes)/room/[code]/page.tsx
+++ cm-app/app/(main)/(routes)/room/[code]/page.tsx
@@ -2,14 +2,14 @@ import { RoomTest } from '@/components/room-tests/room-test';
 import CurrentProfile from '@/lib/current-profile';
 import { RedirectToSignIn } from '@clerk/nextjs';
 
-export default async function RoomPage({ params }: { params: { code: string } }) {
+export default async function RoomPage({ params }: { params: Promise<{ code: string }> }) {
     const user = await CurrentProfile();
 
     if (!user) {
         return <RedirectToSignIn />;
     }
 
-    const { code } = params;
+    const { code } = await params;
     const roomUrl = `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/room/${code}`;
     return <RoomTest user={user} roomUrl={roomUrl} code={code} />;
 }
