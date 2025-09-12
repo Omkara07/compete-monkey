386cd80b5ca1dec1b0cf042ccc1d46b3d46f02f4
Omkara07 <singh.omkara07@gmail.com>
2025-09-12 11:09:43 +0530

    enable vercel analytics

--- cm-app/app/layout.tsx
+++ cm-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from '@vercel/analytics/next';
 import { dark } from '@clerk/themes'
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import Script from "next/script";
@@ -97,6 +98,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
